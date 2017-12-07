4fb84d2b9ce5214b33fbc7d762992bc0f40823c6
sangwoo <mesyu17539@gmail.com>
2017-12-07 18:52:58 +0900

    오후 6:12 2017-12-07

--- src/com/sktelecom/jse/serviceImpl/ServiceImpl.java
+++ src/com/sktelecom/jse/serviceImpl/ServiceImpl.java
@@ -1,6 +1,5 @@
 package com.sktelecom.jse.serviceImpl;
 
-import java.util.Random;
 
 import com.sktelecom.jse.domain.MemberBean;
 import com.sktelecom.jse.domain.SkPhoneBean;
@@ -10,12 +9,10 @@ import com.sktelecom.jse.service.Service;
 public class ServiceImpl implements Service{
 	private static String GR="galaxy", IPH="iphone";
 	private int memberCount, skcount; 
-	SkPhoneBean bean=null;
-	MemberBean mbean=null;
-	Random ra=null;
 	MemberBean[] mbeans;
 	SkPhoneBean[] sbeans;
 	public ServiceImpl() {
+		memberCount=skcount=0;
 		mbeans=new MemberBean[2];
 		sbeans=new SkPhoneBean[2];
 	}
@@ -23,24 +20,18 @@ public class ServiceImpl implements Service{
 		this.mbeans[memberCount++]=mBean;
 		this.sbeans[skcount++]=sBean;
 	}
-	public String openService(SkPhoneBean nbean, MemberBean memberBean) {
-		mbean=new MemberBean();
-		bean=new SkPhoneBean();
-		ra=new Random();
-		mbean.setName(memberBean.getName());
-		String brand=nbean.getBrand();
-		bean.setBrand(brand.equals("1")?GR:brand.equals("2")?IPH:"없음");
-		Random ra=new Random();
+	public String openService(SkPhoneBean skbean, MemberBean mBean) {
+		String brand=skbean.getBrand();
+		skbean.setBrand(brand.equals("1")?GR:brand.equals("2")?IPH:"없음");
 		String num="";
 		for(int i=0;i<8;i++) {
-			num+=ra.nextInt(9);
+			num+=(int)(Math.random()*10);
 			if(i==3) {
 				num+="-";
 			}
 		}
-		bean.setNumber(String.format("010-%s",num));
-		addmem(mbean,bean);
-		return String.format("[%s]님 이름으로 [%S] [%s]폰이 개통되었습니다", mbean.getName(), bean.getNumber(),bean.getBrand());
+		skbean.setNumber(String.format("010-%s",num));
+		addmem(mBean,skbean);
+		return String.format("[%s]님 이름으로 [%S] [%s]폰이 개통되었습니다", mBean.getName(), skbean.getNumber(),skbean.getBrand());
 	}
-
 }
